fc85de2dc8a442f42fdc94eb8ba3f80a4271ba70
Reaster <darcrai11@gmail.com>
2022-08-24 12:04:45 +0200

    added basic buttons in the popup

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,1 +1,10 @@
-browser.tabs.executeScript({file: "/content_scripts/picker.js"})
+//browser.tabs.executeScript({file: "/content_scripts/picker.js"})
+
+console.log("popup launched")
+
+function init () {
+	document.getElementById('menu_injector').addEventListener('click', () => {console.log('lol')}) //TODO launch coresponding function
+	document.getElementById('toggle_highlight').addEventListener('click', () => {console.log('lol2')}) //TODO launch coresponding function
+}
+
+document.addEventListener('DOMContentLoaded', init)
