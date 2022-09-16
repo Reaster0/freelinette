33c39483fffee8541a9a200674e1c52a91425b00
Reaster <darcrai11@gmail.com>
2022-09-16 14:00:34 +0200

    try to make this stupid exec work

--- backend/nestJS/docker-entrypoint.sh
+++ backend/nestJS/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir /cypress
+chown -R 1000:1000 /cypress
+export CYPRESS_CACHE_FOLDER=/cypress/.cache
 /app/cypress-runtime/node_modules/.bin/cypress install
+# chown -R root.root /root/.cache/Cypress
 
 tail -f /dev/null
