b30ae6ce085cb5122c8b7ad36301d602a02e3a97
Reaster <darcrai11@gmail.com>
2022-09-19 15:30:33 +0200

    little fix for the docker cgi-cypress

--- backend/CGI-Cypress/docker-entrypoint.sh
+++ backend/CGI-Cypress/docker-entrypoint.sh
@@ -2,8 +2,7 @@
 chown -R www-data:www-data /app/cypress
 chown -R www-data:www-data /app/.cache
 source /etc/apache2/envvars
-# /app/cypress-runtime/node_modules/.bin/cypress install
 
 echo "Starting Apache"
 apache2 -D FOREGROUND
-# tail -f /dev/null
+#tail -f /dev/null
