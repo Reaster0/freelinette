e5c634e51d719cc1560275ddc243328c1a37fd53
Reaster <darcrai11@gmail.com>
2022-09-26 10:45:45 +0200

    remove the cgi-cypress container

--- backend/CGI-Cypress/cypress-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "Content-type: text/html\n\n"
-
-cd /app
-export CYPRESS_CACHE_FOLDER=/app/.cache
-./node_modules/.bin/cypress run
--- backend/CGI-Cypress/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chown -R www-data:www-data /app/cypress
-chown -R www-data:www-data /app/.cache
-source /etc/apache2/envvars
-
-echo "Starting Apache"
-apache2 -D FOREGROUND
-#tail -f /dev/null
